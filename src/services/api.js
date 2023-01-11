4de9164a6b3e4fdfc660792340485aba78cee589
bruhleal <bruna11leal@live.com>
2023-01-11 17:12:18 -0400

    testando req8

--- src/services/api.js
+++ src/services/api.js
@@ -1,11 +1,12 @@
 // Requisição do token para API
-const searchToken = async => {
+const searchToken = async () => {
   const { history } = this.props;
 
   const responseAPI = await fetch('https://opentdb.com/api_token.php?command=request');
-  const { token } = await responseAPI.json();
+  const responseJson = await responseAPI.json();
+  const { token } = responseJson;
   localStorage.setItem('token', token);
   history.push('/game');
-}
+};
 
 export default searchToken;
