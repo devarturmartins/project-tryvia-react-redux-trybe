d1ad80f99db3136be59a898307431065c520b84b
Danilo <danilo_dias07@hotmail.com>
2023-01-14 17:12:08 -0300

    tentativa do formulario

--- src/pages/Feedback.jsx
+++ src/pages/Feedback.jsx
@@ -1,12 +1,37 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import Header from '../components/Header';
 
 class Feedback extends Component {
   render() {
+    const { rights, score } = this.props;
+    const minRights = 3;
     return (
-      <Header />
+      <>
+        <Header />
+        <p data-testid="feedback-text">
+          {rights < minRights ? 'Could be better...' : 'Well Done!'}
+        </p>
+        <div>
+          <p>Seu score é:</p>
+          <p data-testid="feedback-total-score">
+            { score }
+          </p>
+        </div>
+        <div>
+          <p>Seus acertos foram de:</p>
+          <p data-testid="feedback-total-question">
+            { rights }
+          </p>
+        </div>
+      </>
     );
   }
 }
+
+Feedback.propTypes = {
+  rights: PropTypes.number.isRequired,
+  score: PropTypes.number.isRequired,
+};
 
 export default Feedback;
